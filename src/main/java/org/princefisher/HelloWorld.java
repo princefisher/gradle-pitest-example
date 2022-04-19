be5f222c5273043b073fe4b5d51adde6d31e83b8
Constantin Eisinger <constantin.eisinger@sap.com>
2022-04-19 08:54:01 -0700

    Adding more examples for mutations

--- src/main/java/org/princefisher/HelloWorld.java
+++ src/main/java/org/princefisher/HelloWorld.java
@@ -10,12 +10,28 @@ public class HelloWorld {
   @Override
   public String toString() {
     var sb = new StringBuilder();
-    boolean isAGoodDay = false;
     if (isGoodDay) {
       sb.append("HelloWorld, today is a good day!");
     } else {
       sb.append("The apocalypse is near...");
     }
+
+    if (isGoodDay) {
+      // everything is fine
+      flip();
+    } else {
+      // not so fine anymore
+      return "you failed this mutation...";
+    }
+
     return sb.toString();
+  }
+
+  private void flip() {
+    isGoodDay = !isGoodDay;
+  }
+
+  public boolean isGoodDay() {
+    return isGoodDay;
   }
 }
--- src/test/java/org/princefisher/HelloWorldTest.java
+++ src/test/java/org/princefisher/HelloWorldTest.java
@@ -10,11 +10,15 @@ class HelloWorldTest {
   @Test
   void testGoodDay() {
     var cut = new HelloWorld(true);
+//    assertTrue(cut.isGoodDay());
     assertEquals("HelloWorld, today is a good day!", cut.toString());
+//    assertFalse(cut.isGoodDay());
   }
+
   @Test
   void testBadDay() {
     var cut = new HelloWorld(false);
-    assertEquals("The apocalypse is near...", cut.toString());
+//    assertEquals("The apocalypse is near...", cut.toString());
+    assertEquals("you failed this mutation...", cut.toString());
   }
 }
